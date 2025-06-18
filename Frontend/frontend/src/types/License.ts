640b23279e3f55485940cffe2f8ec78412dae094
Kevinbrunas <kevinbrunas@hotmail.com>
2025-06-18 20:51:37 -0300

    muchos cambios

--- Frontend/frontend/src/types/License.ts
+++ Frontend/frontend/src/types/License.ts
@@ -2,3 +2,16 @@ export type LicenseSubmission = {
   personId: string;
   licenseTypes: string[];
 };
+export interface LicenciaActiva{
+  documentoTitular: string;
+  nombreTitular: string;
+  apellidoTitular: string;
+  clases: string;
+  domicilioTitular: string;
+  fechaEmisionLicencia: string;     // formato ISO: "YYYY-MM-DD"
+  fechaVencimientoLicencia: string; // formato ISO: "YYYY-MM-DD"
+  grupoFactor: string;
+  donanteOrganos: string;           // puede ser "Sí" / "No", ajustalo según backend
+  observacionesLicencia: string;
+}
+
