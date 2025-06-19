fd5db6c681a2245d0a09f0e7ee861b0e5d2ee374
gonzag8 <gonzagaitan8@gmail.com>
2025-06-19 13:02:17 -0300

    DTOs y demas

---
+++ Backend/backend/src/main/java/tp/agil/backend/dtos/LicenciasVencidasDTO.java
@@ -0,0 +1,13 @@
+// Backend/backend/src/main/java/tp/agil/backend/dtos/LicenciasVencidasDTO.java
+package tp.agil.backend.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+public class LicenciasVencidasDTO {
+    private List<LicenciaExpiradaDTO> expiradas;
+    private List<LicenciaActivaDTO> activasVencidas;
+}
