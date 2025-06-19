c62b4433fd3fe31b94000fbd12d122b53c4c3565
Marcos Debona <marcosjd2005@gmail.com>
2025-06-19 00:36:14 -0300

    update emitir

--- Backend/backend/src/main/java/tp/agil/backend/dtos/LicenciaEmitidaDTO.java
+++ Backend/backend/src/main/java/tp/agil/backend/dtos/LicenciaEmitidaDTO.java
@@ -8,13 +8,9 @@ import java.time.LocalDate;
 @Setter
 @Getter
 public class LicenciaEmitidaDTO {
-
-    //Documento del titular
     private String documentoTitular;
-    //Documento del usuario administrativo
     private String documentoUsuario;
     private String observaciones;
-    //string separado por espacios: clases = "A B C D E F G"
     private String clases;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechaEmision;
