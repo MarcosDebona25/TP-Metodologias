c67b57df1f5598e8522265acf59b332bde011554
Gonzalo Gaitan <166561060+gonzag8@users.noreply.github.com>
2025-07-03 02:28:52 -0300

    Update UsuarioService.java

--- Backend/backend/src/main/java/tp/agil/backend/services/UsuarioService.java
+++ Backend/backend/src/main/java/tp/agil/backend/services/UsuarioService.java
@@ -6,4 +6,5 @@ import tp.agil.backend.dtos.UsuarioFormDTO;
 public interface UsuarioService {
     UsuarioDTO crearUsuario(UsuarioFormDTO usuarioFormDTO);
     UsuarioDTO actualizarUsuario(UsuarioFormDTO usuarioFormDTO);
+    UsuarioDTO getUsuarioByNumeroDocumento(String numeroDocumento);
 }
